f3e7961b6c69e0e43297715bfe51fdea3e1f9a03
unknown <kristenn.hsieh@gmail.com>
2021-04-18 14:45:13 +0800

    Add several examples

---
+++ list.py
@@ -0,0 +1,11 @@
+#Define a list of names
+names = ["Harry", "Ron", "Hermione", "Ginny"]
+
+print("original list : ", names)
+print("first element: ", names[0])
+
+names.append("Draco")
+
+names.sort()
+
+print("after appended \"Draco\" and sorted: ", names)
